197f1eb655fa358ec436cdff93e94889073b3a05
lucas <lucas.campestrini@gmail.com>
2023-02-13 18:38:56 -0300

    Revamped the css and added a check to finish tasks

--- behavior/todo.js
+++ behavior/todo.js
@@ -13,31 +13,37 @@ var todo = {
     onChange:function(){
         var lista = document.getElementById("todo_list");
         lista.childNodes.forEach(todo._removeEmptyItems);
-        if(lista.lastChild.firstChild.value != ""){
+        if(lista.lastChild.lastChild.value != ""){
             todo._addItem();
         }
         debugger;
     },
 
     onCheck:function(){
-
+        debugger;
+        if(this.value == 'on'){
+            this.parentElement.lastChild.disabled = true;
+        } else {
+            this.parentElement.lastChild.disabled = false;
+        }
     },
 
     _addItem:function(){
         var lista = document.getElementById("todo_list");
         var item = document.createElement("li");
-        var input = document.createElement("input");
         var check = document.createElement("input");
+        var input = document.createElement("input");
         check.setAttribute("type", "checkbox");
         input.addEventListener("change", todo.onChange);
-        item.appendChild(input);
+        check.addEventListener("select", todo.onCheck);
         item.appendChild(check);
+        item.appendChild(input);
         lista.appendChild(item);
         input.focus();
     },
 
     _removeEmptyItems:function(item){
-        if(item.firstChild.value == ""){
+        if(item.lastChild.value == ""){
             item.remove();
         }
     }
