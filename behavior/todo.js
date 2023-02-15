6d17ce59ec2008455712436f5d79e51251d33bf9
lucas <lucas.campestrini@gmail.com>
2023-02-15 20:57:06 -0300

    created function that adds a new item to the task list

--- behavior/todo.js
+++ behavior/todo.js
@@ -2,30 +2,35 @@ var todo = {};
 
 todo.taskList = [];
 
-todo.editTask = function(){
+todo.editTask = function () {
 
 };
 
-todo.removeTask = function(){
+todo.removeTask = function () {
 
 };
 
-todo.addTask = function(){
+todo.addTask = function () {
+    var newTask = todo._getNewEmptyTask();
+    var tasklist = document.getElementsById("task_list");
+    var newTaskDescription = document.getElementsById("new_task_description").value;
 
+    newTask.firstChild.value = newTaskDescription.value;
+    tasklist.appendChild(newTask);
+    
+    return newTask;
 };
 
-todo._getNewEmptyTask = function(){
-    var item = {};
-    var li = document.createElement("li");
-    var textInput = document.createElement("input");
-    var checkBoxInput = document.createElement("input");
-    
-    textInput.setAttribute("type", "text");
-    checkBoxInput.setAttribute("type", "checkbox");
-    
-    item.li = li;
-    item.text = textInput;
-    item.check = checkBoxInput;
-    
+todo._getNewEmptyTask = function () {
+    var item = document.createElement("li");
+    var descriptionTextInput = document.createElement("input");
+    var doneInputCheck = document.createElement("input");
+
+    descriptionTextInput.setAttribute("type", "text");
+    doneInputCheck.setAttribute("type", "checkbox");
+
+    item.appendChild(descriptionTextInput);
+    item.appendChild(doneInputCheck);
+
     return item;
 };
