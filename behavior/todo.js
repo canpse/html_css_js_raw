f42509ab41a3593908fabf9f1836675a6799e8ef
lucas <lucas.campestrini@gmail.com>
2023-02-15 20:14:34 -0300

    Starting over, new idea for the logic

--- behavior/todo.js
+++ behavior/todo.js
@@ -1,78 +1,19 @@
-var todo = {
-    // functions preceded with _ are to be treated as private
+var todo = {};
 
-    todoList: JSON.parse(localStorage.getItem('todoList')) || [],
+todo.taskList = [];
 
-    onLoad:function(){
-        var list = document.getElementById("todo_list");
-        todo._loadFromStorage();
-        if(list.childNodes.length < 1 || list.lastChild.lastChild.value != ""){
-            todo._addEmptyItem();
-        }
-    },
+todo.editTask = function(){
 
-    onChange:function(){
-        var list = document.getElementById("todo_list");
-        list.childNodes.forEach(todo._removeEmptyItems);
-        if(list.lastChild.lastChild.value != ""){
-            todo._addTodoToLocalStorage.call(this);
-            todo._addEmptyItem();
-        }
-    },
+};
 
-    onCheck:function(){
-        debugger;
-        if(this.value == 'on'){
-            this.parentElement.lastChild.disabled = true;
-        } else {
-            this.parentElement.lastChild.disabled = false;
-        }
-        todo._addTodoToLocalStorage.call(this);
-    },
+todo.removeTask = function(){
 
-    _addEmptyItem:function(){
-        var list = document.getElementById("todo_list");
-        var item = document.createElement("li");
-        var check = document.createElement("input");
-        var input = document.createElement("input");
-        check.setAttribute("type", "checkbox");
-        input.addEventListener("change", todo.onChange);
-        check.addEventListener("select", todo.onCheck);
-        item.appendChild(check);
-        item.appendChild(input);
-        list.appendChild(item);
-        input.focus();
-        return item;
-    },
+};
 
-    _addTodoToLocalStorage:function(){
-        var item = this.parentElement;
-        todo.todoList.push({"done":item.firstChild.value, "task":item.lastChild.value});
-        localStorage.setItem('todoList', JSON.stringify(todo.todoList));
-    },
+todo.addTask = function(){
 
-    _removeEmptyItems:function(item){
-        if(item.lastChild.value == ""){
-            todo._removeFromStorage.call(item);
-            item.remove();
-        }
-    },
+};
 
-    _loadFromStorage:function(){
-        var list = document.getElementById('todo_list');
-        list.innerHTML = '';
-        for (var i = 0; i < todo.todoList.length; i++) {
-            var item = todo._addEmptyItem();
-            item.lastChild.value = todo.todoList[i].task;
-            item.firstChild.value = todo.todoList[i].done;
-        }
-    },
-
-    _removeFromStorage:function(){
-        var item = this;
-        var index = Array.prototype.indexOf.call(item.parentElement.children, item);
-        todo.todoList.splice(index, 1);
-        localStorage.setItem('todoList', JSON.stringify(todo.todoList));
-    }
-
-}
+todo._getNewEmptyTaskFields = function(){
+    var input = document.createElement("input");
+};
